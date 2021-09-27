21a92b51e47c1b787cfb79e3e142bf22175af8d3
lsj1759 <lsj1759@naver.com>
2021-09-27 11:32:03 +0900

    홀수 짝수 판별하기

--- 21.09.27/source.cpp
+++ 21.09.27/source.cpp
@@ -2,31 +2,47 @@
 
 using namespace std;
 
+/*int main()
+{
+	//비교 연산
+	// >, <, >=, <=. ++, !=
+	
+	int Number = 0;
+	
+	Reinput:
+	cin >> Number;
+	// 1 <= Number <= 9
+	// 논리 연산자
+	// And &&
+	// Or ||
+	// Not !
+	if (1 <= Number && Number <= 9)
+	{
+		cout << "구구단에 적합한 수입니다." << endl;
+	}
+	else
+	{
+		cout << "숫자를 다시 입력해주세요.(1 ~ 9)" << endl;
+		goto Reinput;
+	}
+	
+	
+	
+	return 0;
+}*/
+
 int main()
 {
 	int Number = 0;
-
-	cout << "출력할 구구단을 입력하세요. : ";
+	cout << "숫자를 입력하세요 : ";
 	cin >> Number;
-	for (int i = 0; i <= 9; i++)
+
+	if (Number %2 == 1)
 	{
-		cout << Number << " X " << i << " = " << Number * i << endl;
+		cout << "홀수 입니다." << endl;
 	}
-
-	cout << endl;
-	cout << "----------------------------------------------" << endl;
-	cout << endl;
-	
-	for (int j = 1; j <= 9; j++)
+	else
 	{
-		for (int i = 1; i <= 9; i++)
-		{
-			cout << j << " X " << i << " = " << j * i << endl;
-		}
+		cout << "짝수입니다." << endl;
 	}
- 
-
-
-
-	return 0;
 }
