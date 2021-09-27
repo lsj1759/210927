342905b54119616f1ddfc7a2b97aab1de7fb345b
lsj1759 <lsj1759@naver.com>
2021-09-27 10:24:26 +0900

    구구단 출력하기

--- 21.09.27/source.cpp
+++ 21.09.27/source.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
+	int Number = 0;
+
+	cout << "출력할 구구단을 입력하세요. : ";
+	cin >> Number;
+	for (int i = 0; i <= 9; i++)
+	{
+		cout << Number << " X " << i << " = " << Number * i << endl;
+	}
+
+	cout << endl;
+	cout << "----------------------------------------------" << endl;
+	cout << endl;
+	
+	for (int j = 1; j <= 9; j++)
+	{
+		for (int i = 1; i <= 9; i++)
+		{
+			cout << j << " X " << i << " = " << j * i << endl;
+		}
+	}
+ 
+
+
+
 	return 0;
 }
